8fc54f4dee9483ce7d06e18f0784e86143820bbd
ANITA-16 <anacristinaguamangonzalez@gmail.com>
2023-04-13 00:19:09 -0500

    Demo2

---
+++ Demo2/src/demo2/Demo2.java
@@ -0,0 +1,11 @@
+package demo2;
+
+public class Demo2 {
+    
+    public static void main(String[] args) {
+        // Taller00
+        String mensaje = "Loja, una ciudad llena de arte y naturaleza con 400322";
+        System.out.printf("%s",mensaje);
+    }
+    
+}
